3a656ad632bb53889e7c1f839eae21af21a82ec7
ChaseRuff <112829224+ChaseRuff@users.noreply.github.com>
2024-11-02 13:18:43 +0300

    Delete launcher.ps1

--- launcher.ps1
+++
@@ -1,4 +0,0 @@
-# launcher.ps1
-
-# Загружаем и запускаем меню в новой консоли PowerShell
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "irm 'https://ссылка_на_ваш_скрипт/menu.ps1' | iex"
